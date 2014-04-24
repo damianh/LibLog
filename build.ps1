a04ccb9ae622f59cedcbf5d7a2261b7243cd7579
Damian Hickey <dhickey@gmail.com>
2014-04-24 12:06:02 +0200

    Delete confusing stuff. ARE YOU HAPPY NOW JONATHAN???

--- build.ps1
+++
@@ -1,4 +0,0 @@
-param( [int]$buildNumber = 0 )
-Import-Module .\tools\psake\psake.psm1
-Invoke-Psake .\default.ps1 default -framework "4.0" -properties @{ buildNumber=$buildNumber }
-Remove-Module psake
