f7bc6e6860f6f60c68fea4109a46333312f8afed
Damian Hickey <dhickey@gmail.com>
2014-09-14 10:12:44 +0200

    Using fluent assertions

--- src/LibLog.Tests/LogProviderTests.cs
+++ src/LibLog.Tests/LogProviderTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace LibLog.Logging
 {
     using System;
+    using FluentAssertions;
     using LibLog.Logging.LogProviders;
     using Xunit;
 
@@ -13,7 +14,8 @@
             NLogLogProvider.ProviderIsAvailableOverride = true;
             Log4NetLogProvider.ProviderIsAvailableOverride = true;
             ILog logger = LogProvider.GetCurrentClassLogger();
-            Assert.IsType<NLogLogProvider.NLogLogger>(((LoggerExecutionWrapper)logger).WrappedLogger);
+
+            ((LoggerExecutionWrapper) logger).WrappedLogger.Should().BeOfType<NLogLogProvider.NLogLogger>();
         }
 
         [Fact]
@@ -23,29 +25,46 @@
             NLogLogProvider.ProviderIsAvailableOverride = false;
             Log4NetLogProvider.ProviderIsAvailableOverride = true;
             ILog logger = LogProvider.GetLogger(GetType());
-            Assert.IsType<Log4NetLogProvider.Log4NetLogger>(((LoggerExecutionWrapper)logger).WrappedLogger);
+
+            ((LoggerExecutionWrapper)logger).WrappedLogger.Should().BeOfType<Log4NetLogProvider.Log4NetLogger>();
         }
 
         [Fact]
-        public void When_neither_NLog_or_Log4Net_is_available_Then_should_get_EntLibLogger()
+        public void When_neither_NLog_nor_Log4Net_is_available_Then_should_get_EntLibLogger()
         {
             LogProvider.SetCurrentLogProvider(null);
             NLogLogProvider.ProviderIsAvailableOverride = false;
             Log4NetLogProvider.ProviderIsAvailableOverride = false;
             EntLibLogProvider.ProviderIsAvailableOverride = true;
             ILog logger = LogProvider.GetLogger(GetType());
-            Assert.IsType<EntLibLogProvider.EntLibLogger>(((LoggerExecutionWrapper)logger).WrappedLogger);
+
+            ((LoggerExecutionWrapper)logger).WrappedLogger.Should().BeOfType<EntLibLogProvider.EntLibLogger>();
         }
 
         [Fact]
-        public void When_neither_NLog_or_Log4Net_or_EntLib_is_available_Then_should_get_NoOpLogger()
+        public void When_neither_NLog_nor_Log4Net_nor_EntLib_is_available_Then_should_get_SeriLogLogger()
         {
             LogProvider.SetCurrentLogProvider(null);
             NLogLogProvider.ProviderIsAvailableOverride = false;
             Log4NetLogProvider.ProviderIsAvailableOverride = false;
             EntLibLogProvider.ProviderIsAvailableOverride = false;
+            SerilogLogProvider.ProviderIsAvailableOverride = true;
             ILog logger = LogProvider.GetLogger(GetType());
-            Assert.IsType<LogProvider.NoOpLogger>(logger);
+
+            ((LoggerExecutionWrapper)logger).WrappedLogger.Should().BeOfType<SerilogLogProvider.SerilogLogger>();
+        }
+
+        [Fact]
+        public void When_neither_NLog_nor_Log4Net_or_EntLib_nor_Serilog_is_available_Then_should_get_NoOpLogger()
+        {
+            LogProvider.SetCurrentLogProvider(null);
+            NLogLogProvider.ProviderIsAvailableOverride = false;
+            Log4NetLogProvider.ProviderIsAvailableOverride = false;
+            EntLibLogProvider.ProviderIsAvailableOverride = false;
+            SerilogLogProvider.ProviderIsAvailableOverride = false;
+            ILog logger = LogProvider.GetLogger(GetType());
+
+            logger.Should().BeOfType<LogProvider.NoOpLogger>();
         }
 
         public void Dispose()
@@ -53,6 +72,7 @@
             NLogLogProvider.ProviderIsAvailableOverride = true;
             Log4NetLogProvider.ProviderIsAvailableOverride = true;
             EntLibLogProvider.ProviderIsAvailableOverride = true;
+            SerilogLogProvider.ProviderIsAvailableOverride = true;
         }
     }
 }
