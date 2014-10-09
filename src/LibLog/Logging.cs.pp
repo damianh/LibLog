af593b7a9f28c896fce50581e24cb2f6ef16f610
Robin van der Knaap <robin@skaele.nl>
2014-10-09 09:31:31 +0200

    Also fixed template

--- src/LibLog/Logging.cs.pp
+++ src/LibLog/Logging.cs.pp
@@ -326,6 +326,11 @@ namespace $rootnamespace$.Logging
 
         public bool Log(LogLevel logLevel, Func<string> messageFunc)
         {
+			if (messageFunc == null)
+            {
+                return _logger.Log(logLevel, null);
+            }
+
             Func<string> wrappedMessageFunc = () =>
             {
                 try
