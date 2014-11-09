26639e68ad6a88b0f0513005c7ef7c8a909f2bd6
Kendall Miller <kendall@gibraltarsoftware.com>
2014-11-09 18:22:37 -0500

    Added support for Loupe

--- src/LibLog.Tests/LogProviderTests.cs
+++ src/LibLog.Tests/LogProviderTests.cs
@@ -13,6 +13,9 @@
             LogProvider.SetCurrentLogProvider(null);
             NLogLogProvider.ProviderIsAvailableOverride = true;
             Log4NetLogProvider.ProviderIsAvailableOverride = true;
+            EntLibLogProvider.ProviderIsAvailableOverride = true;
+            SerilogLogProvider.ProviderIsAvailableOverride = true;
+            LoupeLogProvider.ProviderIsAvailableOverride = true; 
             ILog logger = LogProvider.GetCurrentClassLogger();
 
             ((LoggerExecutionWrapper)logger).WrappedLogger.Should().BeOfType<NLogLogProvider.NLogLogger>();
@@ -24,6 +27,9 @@
             LogProvider.SetCurrentLogProvider(null);
             NLogLogProvider.ProviderIsAvailableOverride = false;
             Log4NetLogProvider.ProviderIsAvailableOverride = true;
+            EntLibLogProvider.ProviderIsAvailableOverride = true;
+            SerilogLogProvider.ProviderIsAvailableOverride = true;
+            LoupeLogProvider.ProviderIsAvailableOverride = true;
             ILog logger = LogProvider.For<LogProviderTests>();
 
             ((LoggerExecutionWrapper)logger).WrappedLogger.Should().BeOfType<Log4NetLogProvider.Log4NetLogger>();
@@ -36,6 +42,8 @@
             NLogLogProvider.ProviderIsAvailableOverride = false;
             Log4NetLogProvider.ProviderIsAvailableOverride = false;
             EntLibLogProvider.ProviderIsAvailableOverride = true;
+            SerilogLogProvider.ProviderIsAvailableOverride = true;
+            LoupeLogProvider.ProviderIsAvailableOverride = true;
             ILog logger = LogProvider.For<LogProviderTests>();
 
             ((LoggerExecutionWrapper)logger).WrappedLogger.Should().BeOfType<EntLibLogProvider.EntLibLogger>();
@@ -49,31 +57,38 @@
             Log4NetLogProvider.ProviderIsAvailableOverride = false;
             EntLibLogProvider.ProviderIsAvailableOverride = false;
             SerilogLogProvider.ProviderIsAvailableOverride = true;
+            LoupeLogProvider.ProviderIsAvailableOverride = true;
             ILog logger = LogProvider.For<LogProviderTests>();
 
             ((LoggerExecutionWrapper)logger).WrappedLogger.Should().BeOfType<SerilogLogProvider.SerilogLogger>();
         }
 
         [Fact]
-        public void When_neither_NLog_nor_Log4Net_or_EntLib_nor_Serilog_is_available_Then_should_get_NoOpLogger()
+        public void When_neither_NLog_nor_Log4Net_or_EntLib_nor_Serilog_is_available_Then_should_get_LoupeLogger()
         {
             LogProvider.SetCurrentLogProvider(null);
             NLogLogProvider.ProviderIsAvailableOverride = false;
             Log4NetLogProvider.ProviderIsAvailableOverride = false;
             EntLibLogProvider.ProviderIsAvailableOverride = false;
             SerilogLogProvider.ProviderIsAvailableOverride = false;
+            LoupeLogProvider.ProviderIsAvailableOverride = true;
             ILog logger = LogProvider.For<LogProviderTests>();
 
-            logger.Should().BeOfType<LogProvider.NoOpLogger>();
+            ((LoggerExecutionWrapper)logger).WrappedLogger.Should().BeOfType<LoupeLogProvider.LoupeLogger>();
         }
 
         [Fact]
-        public void When_Serilog_and_Log4Net_is_available_Then_should_get_Serilog()
+        public void When_neither_NLog_nor_Log4Net_or_EntLib_nor_Serilog_nor_Loupe_is_available_Then_should_get_NoOpLogger()
         {
             LogProvider.SetCurrentLogProvider(null);
+            NLogLogProvider.ProviderIsAvailableOverride = false;
+            Log4NetLogProvider.ProviderIsAvailableOverride = false;
+            EntLibLogProvider.ProviderIsAvailableOverride = false;
+            SerilogLogProvider.ProviderIsAvailableOverride = false;
+            LoupeLogProvider.ProviderIsAvailableOverride = false;
             ILog logger = LogProvider.For<LogProviderTests>();
 
-            ((LoggerExecutionWrapper)logger).WrappedLogger.Should().BeOfType<SerilogLogProvider.SerilogLogger>();
+            logger.Should().BeOfType<LogProvider.NoOpLogger>();
         }
 
         public void Dispose()
@@ -82,6 +97,7 @@
             Log4NetLogProvider.ProviderIsAvailableOverride = true;
             EntLibLogProvider.ProviderIsAvailableOverride = true;
             SerilogLogProvider.ProviderIsAvailableOverride = true;
+            LoupeLogProvider.ProviderIsAvailableOverride = true;
         }
     }
 }
