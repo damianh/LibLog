87b619fe8e84c2eba5eff375dfa0da13de14fbab
Damian Hickey <dhickey@gmail.com>
2014-12-10 14:30:06 +0100

    Add comment detailing how IsEnabled functionality is performed via the same Log method

--- src/LibLog/LibLog.cs
+++ src/LibLog/LibLog.cs
@@ -43,9 +43,12 @@ namespace LibLog.Logging
         /// <param name="logLevel">The log level.</param>
         /// <param name="messageFunc">The message function.</param>
         /// <param name="exception">An optional exception.</param>
+        /// <returns>true if the message was logged. Otherwise false.</returns>
         /// <remarks>
         /// Note to implementers: the message func should not be called if the loglevel is not enabled
         /// so as not to incur performance penalties.
+        /// 
+        /// To check IsEnabled call Log with only LogLevel and check the return value, no event will be written
         /// </remarks>
         bool Log(LogLevel logLevel, Func<string> messageFunc, Exception exception = null);
     }
