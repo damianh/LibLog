780aa69f2bd1b063de5fed485e496348f3424539
Damian Hickey <dhickey@gmail.com>
2015-03-28 21:43:37 +0100

    Added Log4Net example.

---
+++ src/LibLog.Example.Library/Foo.cs
@@ -0,0 +1,14 @@
+﻿namespace LibLog.Example.Library
+{
+    using LibLog.Example.Library.Logging;
+
+    public static class Foo
+    {
+        private static readonly ILog Logger = LogProvider.GetCurrentClassLogger();
+
+        public static void Bar()
+        {
+            Logger.Info("Baz");
+        }
+    }
+}
---
+++ src/LibLog.Example.Log4Net/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LibLog.Example.Log4Net
+{
+    using System;
+    using System.IO;
+    using log4net.Config;
+    using LibLog.Example.Library;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+
+            Foo.Bar();
+
+            Console.ReadLine();
+        }
+    }
+}
