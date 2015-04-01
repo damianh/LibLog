64ac921be4ca3554c5905118890db71e85e1a486
Damian Hickey <dhickey@gmail.com>
2015-04-01 11:58:40 +0200

    Update Example Library LibLog to 4.0.1 build 94. Added NLog example project.

---
+++ src/LibLog.Example.NLog/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LibLog.Example.NLog
+{
+    using System;
+    using global::NLog;
+    using global::NLog.Config;
+    using global::NLog.Targets;
+    using LibLog.Example.Library;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var config = new LoggingConfiguration();
+            var consoleTarget = new ColoredConsoleTarget();
+            config.AddTarget("console", consoleTarget);
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, consoleTarget));
+            LogManager.Configuration = config;
+
+            Foo.Bar();
+
+            Console.ReadLine();
+        }
+    }
+}
