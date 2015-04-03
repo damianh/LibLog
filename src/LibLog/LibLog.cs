e152785f697397df7d28137d474962d695c0a626
Adam Ralph <adam@adamralph.com>
2015-04-03 12:10:26 +0200

    add support for LIBLOG_PROVIDERS_ONLY

--- src/LibLog/LibLog.cs
+++ src/LibLog/LibLog.cs
@@ -32,21 +32,40 @@
 // this can have unintendend consequences of consumers of your library using your library to resolve a logger. If the
 // reason is because you want to open this functionality to other projects within your solution,
 // consider [InternalVisibleTo] instead.
+// 
+// Define LIBLOG_PROVIDERS_ONLY if your library provides its own logging API and you just want to use the
+// LibLog providers internally to provide built in support for popular logging frameworks.
 
 #pragma warning disable 1591
 
 // If you copied this file manually, you need to change this namespace so not to clash with other libraries
 // that use LibLog
+#if LIBLOG_PROVIDERS_ONLY
+namespace LibLog.Logging.LibLog
+#else
 namespace LibLog.Logging
+#endif
 {
     using System.Collections.Generic;
-    using LibLog.Logging.LogProviders;
+#if LIBLOG_PROVIDERS_ONLY
+    using global::LibLog.Logging.LibLog.LogProviders;
+#else
+    using global::LibLog.Logging.LogProviders;
+#endif
     using System;
+#if !LIBLOG_PROVIDERS_ONLY
     using System.Diagnostics;
     using System.Runtime.CompilerServices;
+#endif
 
-    public delegate bool Logger(LogLevel logLevel, Func<string> messageFunc, Exception exception = null, params object[] formatParameters);
+#if LIBLOG_PROVIDERS_ONLY
+    internal
+#else
+    public
+#endif
+    delegate bool Logger(LogLevel logLevel, Func<string> messageFunc, Exception exception = null, params object[] formatParameters);
 
+#if !LIBLOG_PROVIDERS_ONLY
     /// <summary>
     /// Simple interface that represent a logger.
     /// </summary>
@@ -68,11 +87,17 @@ namespace LibLog.Logging
         /// </remarks>
         bool Log(LogLevel logLevel, Func<string> messageFunc, Exception exception = null, params object[] formatParameters );
     }
+#endif
 
     /// <summary>
     /// The log level.
     /// </summary>
-    public enum LogLevel
+#if LIBLOG_PROVIDERS_ONLY
+    internal
+#else
+    public
+#endif
+    enum LogLevel
     {
         Trace,
         Debug,
@@ -82,6 +107,7 @@ namespace LibLog.Logging
         Fatal
     }
 
+#if !LIBLOG_PROVIDERS_ONLY
 #if LIBLOG_PUBLIC
     public
 #else
@@ -336,11 +362,17 @@ namespace LibLog.Logging
             return value;
         }
     }
+#endif
 
     /// <summary>
     /// Represents a way to get a <see cref="ILog"/>
     /// </summary>
-    public interface ILogProvider
+#if LIBLOG_PROVIDERS_ONLY
+    internal
+#else
+    public
+#endif
+    interface ILogProvider
     {
         /// <summary>
         /// Gets the specified named logger.
@@ -368,8 +400,14 @@ namespace LibLog.Logging
     /// <summary>
     /// Provides a mechanism to create instances of <see cref="ILog" /> objects.
     /// </summary>
-    public static class LogProvider
+#if LIBLOG_PROVIDERS_ONLY
+    internal
+#else
+    public
+#endif
+    static class LogProvider
     {
+#if !LIBLOG_PROVIDERS_ONLY
         /// <summary>
         /// The disable logging environment variable. If the environment variable is set to 'true', then logging
         /// will be disabled.
@@ -531,12 +569,28 @@ namespace LibLog.Logging
             }
             return CurrentLogProvider.OpenMappedContext(key, value);
         }
+#endif
 
-        internal delegate bool IsLoggerAvailable();
+#if LIBLOG_PROVIDERS_ONLY
+    private
+#else
+    internal
+#endif
+    delegate bool IsLoggerAvailable();
 
-        internal delegate ILogProvider CreateLogProvider();
+#if LIBLOG_PROVIDERS_ONLY
+    private
+#else
+    internal
+#endif
+    delegate ILogProvider CreateLogProvider();
 
-        internal static readonly List<Tuple<IsLoggerAvailable, CreateLogProvider>> LogProviderResolvers =
+#if LIBLOG_PROVIDERS_ONLY
+    private
+#else
+    internal
+#endif
+    static readonly List<Tuple<IsLoggerAvailable, CreateLogProvider>> LogProviderResolvers =
             new List<Tuple<IsLoggerAvailable, CreateLogProvider>>
         {
             new Tuple<IsLoggerAvailable, CreateLogProvider>(SerilogLogProvider.IsLoggerAvailable, () => new SerilogLogProvider()),
@@ -547,6 +601,7 @@ namespace LibLog.Logging
             new Tuple<IsLoggerAvailable, CreateLogProvider>(ColouredConsoleLogProvider.IsLoggerAvailable, () => new ColouredConsoleLogProvider()),
         };
 
+#if !LIBLOG_PROVIDERS_ONLY
         private static void RaiseOnCurrentLogProviderSet()
         {
             if (_onCurrentLogProviderSet != null)
@@ -554,6 +609,7 @@ namespace LibLog.Logging
                 _onCurrentLogProviderSet(_currentLogProvider);
             }
         }
+#endif
 
         internal static ILogProvider ResolveLogProvider()
         {
@@ -581,6 +637,7 @@ namespace LibLog.Logging
             return null;
         }
 
+#if !LIBLOG_PROVIDERS_ONLY
         internal class NoOpLogger : ILog
         {
             internal static readonly NoOpLogger Instance = new NoOpLogger();
@@ -590,8 +647,10 @@ namespace LibLog.Logging
                 return false;
             }
         }
+#endif
     }
 
+#if !LIBLOG_PROVIDERS_ONLY
     internal class LoggerExecutionWrapper : ILog
     {
         private readonly Logger _logger;
@@ -645,9 +704,14 @@ namespace LibLog.Logging
             return _logger(logLevel, wrappedMessageFunc, exception, formatParameters);
         }
     }
+#endif
 }
 
+#if LIBLOG_PROVIDERS_ONLY
+namespace LibLog.Logging.LibLog.LogProviders
+#else
 namespace LibLog.Logging.LogProviders
+#endif
 {
     using System;
     using System.Collections.Generic;
@@ -1865,7 +1929,10 @@ namespace LibLog.Logging.LogProviders
                 setForegroundExpression, colorParameter).Compile();
         }
 
-        public class ColouredConsoleLogger : ILog
+        public class ColouredConsoleLogger
+#if !LIBLOG_PROVIDERS_ONLY
+            : ILog
+#endif
         {
             private readonly string _name;
             private readonly Action<string> _write;
