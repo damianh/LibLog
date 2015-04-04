63b618472d2656bc0e1375e51be36b44d6de5749
Damian Hickey <dhickey@gmail.com>
2015-04-04 11:50:07 +0200

    globall:: not needed

--- default.ps1
+++ default.ps1
@@ -33,7 +33,7 @@ task RunTests -depends Compile {
 task CreatePP {
     (Get-Content $srcDir\$projectName\$projectName.cs) | Foreach-Object {
         $_ -replace 'namespace YourRootNamespace', 'namespace $rootnamespace$' `
-        -replace 'using global::YourRootNamespace', 'using $rootnamespace$'
+        -replace 'using YourRootNamespace', 'using $rootnamespace$'
         } | Set-Content $buildOutputDir\$projectName.cs.pp -Encoding UTF8
 }
 
