e59ee654bbb42ac50f058aab57b7d13c457e5a36
Damian Hickey <dhickey@gmail.com>
2015-06-11 13:47:26 +0200

    BuildScript: Fix package restore.

--- build.ps1
+++ build.ps1
@@ -11,7 +11,7 @@ if(Test-Path Env:\APPVEYOR_BUILD_NUMBER){
 
 src\.nuget\nuget.exe i src\.nuget\packages.config -o src\packages
 
-$packageConfigs = Get-ChildItem . -Recurse | where{$_.Name -like "packages.*.config"}
+$packageConfigs = Get-ChildItem . -Recurse | where{$_.Name -like "packages.*config"}
 foreach($packageConfig in $packageConfigs){
     Write-Host "Restoring" $packageConfig.FullName
     src\.nuget\nuget.exe i $packageConfig.FullName -o src\packages
