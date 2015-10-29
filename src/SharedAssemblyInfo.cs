eaa20886f67cdce02fea82be3e75ea19b20de30c
Damian Hickey <dhickey@gmail.com>
2015-10-29 14:26:51 +0100

    Set version to 4.2.4

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.1.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyVersion("4.2.4.0")]
+[assembly: AssemblyFileVersion("4.2.4.0")]
