6443e4dab499f0ba6296c1f9a0a6b4333d5a97d2
Damian Hickey <dhickey@gmail.com>
2016-03-29 14:10:23 +0200

    UpDisable parallel test execution (static mutable state yo).

--- src/LibLog.Tests.NoLogFwkReferences/Properties/AssemblyInfo.cs
+++ src/LibLog.Tests.NoLogFwkReferences/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using Xunit;
 
 [assembly: AssemblyTitle("LibLog.Tests.NoLogFwkReferences")]
 [assembly: AssemblyDescription("")]
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
--- src/LibLog.Tests/Properties/AssemblyInfo.cs
+++ src/LibLog.Tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using Xunit;
 
 [assembly: AssemblyTitle("LibLog")]
 [assembly: AssemblyDescription("")]
 [assembly: InternalsVisibleTo("LibLog.Tests")]
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
