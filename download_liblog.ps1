b6885549fb508c9b7288ffb9c442bc6c819d35e6
Damian Hickey <dhickey@gmail.com>
2017-03-17 16:44:54 +0100

    Remove the branch name from the url

--- download_liblog.ps1
+++ download_liblog.ps1
@@ -1,4 +1,4 @@
 $rootNamespace = "MyLib"
-$url = "https://raw.githubusercontent.com/damianh/LibLog/maybe-we-just-download-the-class/releases/latest/LibLog.cs"
+$url = "https://raw.githubusercontent.com/damianh/LibLog/releases/latest/LibLog.cs"
 $response = Invoke-WebRequest $url
 $response.Content.Replace("`$rootnamespace`$", $rootNamespace) | Out-File -Encoding "UTF8" "LibLog.cs"
