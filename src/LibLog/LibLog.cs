30e37e0fb23b40e67b60823c654b2dc7df9d36f0
Paul Mcilreavy <paul@mcilreavy.com>
2018-04-18 15:01:40 +1000

    Disable resharper formatting of this file. It's an ugly mess if resharper split this file into separate classes.

--- src/LibLog/LibLog.cs
+++ src/LibLog/LibLog.cs
@@ -25,6 +25,7 @@
 // SOFTWARE.
 //===============================================================================
 
+// @formatter:off — disable resharper formatter after this line
 // ReSharper disable PossibleNullReferenceException
 
 // Define LIBLOG_PORTABLE conditional compilation symbol for PCL compatibility
@@ -2448,3 +2449,4 @@ namespace YourRootNamespace.LibLog.LogProviders
         }
     }
 }
+// @formatter:on — enable resharper formatter after this line
