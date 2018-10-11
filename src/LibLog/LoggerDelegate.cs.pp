732e3babfc953db481067c681dd71a9842dd609f
Symon Rottem <s.rottem@symco.com.au>
2018-10-11 22:45:42 +0200

    Add missing XML comments to public members to prevent warning CS1591: Missing XML comment for publicly visible type or member.

--- src/LibLog/LoggerDelegate.cs.pp
+++ src/LibLog/LoggerDelegate.cs.pp
@@ -4,6 +4,14 @@ namespace $rootnamespace$.Logging
     using System;
 
 #if !LIBLOG_PROVIDERS_ONLY || LIBLOG_PUBLIC
+	/// <summary>
+    /// Logger delegate.
+    /// </summary>
+    /// <param name="logLevel">The log level</param>
+    /// <param name="messageFunc">The message function</param>
+    /// <param name="exception">The exception</param>
+    /// <param name="formatParameters">The format parameters</param>
+    /// <returns>A boolean.</returns>
     public
 #else
     internal
